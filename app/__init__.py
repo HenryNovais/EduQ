b4d43a69d6df59f53daf02a718bb97a6c4d477c4
HenryNovais <henryncalmon05@gmail.com>
2026-01-20 21:34:37 -0300

    corrigindo testes automatizados

--- app/__init__.py
+++ app/__init__.py
@@ -1,39 +1,45 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS # Importante para o frontend acessar
+from flask_cors import CORS
 from config import Config
 
 db = SQLAlchemy()
 
-def create_app(config_class=Config):
+# Alteração aqui: adicione o parâmetro test_config=None
+def create_app(test_config=None):
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
-    app.config.from_object(config_class)
+    
+    if test_config is None:
+        # Se não passar config de teste, usa a config normal (MySQL)
+        app.config.from_object(Config)
+    else:
+        # Se passar config de teste, usa ela (SQLite em memória)
+        app.config.from_mapping(test_config)
     
     db.init_app(app)
-    CORS(app) # Habilita CORS
+    CORS(app)
 
     from app.controllers.auth_controller import auth_bp
     from app.controllers.question_controller import question_bp
     from app.controllers.stats_controller import stats_bp
-
-    app.register_blueprint(stats_bp, url_prefix='/api/stats')
+    
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
     app.register_blueprint(question_bp, url_prefix='/api/questions')
+    app.register_blueprint(stats_bp, url_prefix='/api/stats')
     
-    # Rota para servir o Frontend
     @app.route('/')
     def index():
         from flask import render_template
         return render_template('login.html')
-    
-    @app.route('/register')
-    def register_page():
-        from flask import render_template
-        return render_template('register.html')
 
     @app.route('/dashboard')
     def dashboard():
         from flask import render_template
         return render_template('dashboard.html')
+
+    @app.route('/register')
+    def register_page():
+        from flask import render_template
+        return render_template('register.html')
 
     return app
