2415b9438df2cac0dbf3105fde26e52af6b709a6
HenryNovais <henryncalmon05@gmail.com>
2026-01-24 16:25:47 -0300

    Auto-setup do banco de dados

--- run.py
+++ run.py
@@ -1,4 +1,27 @@
-from app import create_app
+from app import create_app, db
+from app.models.models import User
+from werkzeug.security import generate_password_hash
+
 app = create_app()
-if __name__ == '__main__':
+
+# --- BLOCO MÁGICO DE INICIALIZAÇÃO (PARA O RENDER) ---
+with app.app_context():
+    # 1. Cria as tabelas do banco (se não existirem)
+    db.create_all()
+    
+    # 2. Verifica se o Admin já existe. Se não, cria ele.
+    if not User.query.filter_by(email="henryncalmon05@gmail.com").first():
+        print("Criando usuário Admin inicial...")
+        admin = User(
+            name="Henry Novais", 
+            email="henryncalmon05@gmail.com", 
+            password_hash=generate_password_hash("123456"), # <--- Coloque sua senha aqui
+            is_admin=True
+        )
+        db.session.add(admin)
+        db.session.commit()
+        print("Admin criado com sucesso!")
+# ---------------------------------------------------------
+
+if __name__ == "__main__":
     app.run(debug=True)
