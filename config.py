7d5bf05686e9d47f319968a35e7697bc53dc4791
HenryNovais <henryncalmon05@gmail.com>
2026-01-24 17:51:46 -0300

    Configurando banco local MySQL e remoto PostgreSQL

--- config.py
+++ config.py
@@ -7,7 +7,12 @@ class Config:
     database_url = os.environ.get('DATABASE_URL')
 
     if database_url and database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
+        # MUNDO DO RENDER (Produção)
+        SQLALCHEMY_DATABASE_URI = database_url.replace("postgres://", "postgresql://", 1)
+    else:
+        # MUNDO LOCAL (Sua Máquina) - Coloque aqui sua conexão MySQL antiga!
+        # Exemplo: mysql+pymysql://root:sua_senha@localhost/vestibular_db
+        SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost/vestibular_db'
 
     SQLALCHEMY_DATABASE_URI = database_url or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
