1200ba2256840ac9aa7310cc64ae350710fce2ae
Henry Novais Calmon <henryncalmon05@gmail.com>
2026-02-19 12:27:56 -0300

    Refactor initialization block and user creation

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ from werkzeug.security import generate_password_hash
 
 app = create_app()
 
-# --- BLOCO MÁGICO DE INICIALIZAÇÃO (PARA O RENDER) ---
 with app.app_context():
     # 1. Cria as tabelas do banco (se não existirem)
     db.create_all()
@@ -15,17 +14,15 @@ with app.app_context():
         admin = User(
             name="Henry Novais", 
             email="henryncalmon05@gmail.com", 
-            password_hash=generate_password_hash("123456"), # <--- Coloque sua senha aqui
+            password_hash=generate_password_hash("123456"),
             is_admin=True
         )
         db.session.add(admin)
         db.session.commit()
         print("Admin criado com sucesso!")
-# ---------------------------------------------------------
 
 if __name__ == "__main__":
     app.run(debug=True)
-    # ADICIONE ESTAS DUAS LINHAS:
     print("--- CONECTANDO EM: ---")
     print(app.config.get('SQLALCHEMY_DATABASE_URI'))
     print("----------------------")
